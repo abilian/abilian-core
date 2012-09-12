b744cd59adf6ab5c7383d507f7ebb71f2ad3120c
Stefane Fermigier <sf@fermigier.com>
2012-09-12 11:25:34 +0200

    Clean up deps management.

--- setup.py
+++ setup.py
@@ -6,26 +6,29 @@ deps = [ line.strip()
          for line in open("deps.txt")
          if line and not line.startswith("#") ]
 
-setup(
-    name='Yaka Core',
-    version='0.1dev',
-    url='http://www.yaka.biz/',
-    license='LGPL',
-    author='Stefane Fermigier',
-    author_email='sf@fermigier.com',
-    description='Enterprise social networking meets CRM',
-    long_description=__doc__,
-    packages=find_packages(".", exclude=['tests.*']),
-    zip_safe=False,
-    platforms='any',
-    install_requires=deps,
-    include_package_data=True,
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
+metadata = dict(
+  name='Yaka Core',
+  version='0.1dev',
+  url='http://www.yaka.biz/',
+  license='LGPL',
+  author='Stefane Fermigier',
+  author_email='sf@fermigier.com',
+  description='Enterprise social networking meets CRM',
+  long_description=__doc__,
+  packages=['yaka'],
+  platforms='any',
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
     ],
+  # Unsupported by distutils.
+  #install_requires=deps,
+  #include_package_data=True,
+  #zip_safe=False,
 )
+
+setup(**metadata)
