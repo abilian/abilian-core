1d20a9c1fa32152b5bc65993de3aad1d007e682c
Stefane Fermigier <sf@fermigier.com>
2012-11-14 21:02:43 +0100

    Work on widgets.

--- tests/unit/test_widgets.py
+++ tests/unit/test_widgets.py
@@ -1,8 +1,9 @@
 # coding=utf-8
+import os
 
 from unittest import TestCase
 from flask import Flask
-from yaka.web.widgets import TableView
+from yaka.web.widgets import TableView, SingleView, Panel, Row
 
 
 class Model(object):
@@ -13,18 +14,34 @@ class Model(object):
 
 class TableViewTestCase(TestCase):
 
-  def test(self):
-    app = Flask(__name__)
-    with app.test_request_context():
+  def setUp(self):
+    # Hack to set up the template folder properly.
+    template_dir = os.path.dirname(__file__) + "/../../yaka/templates"
+    template_dir = os.path.normpath(template_dir)
+    self.app = Flask(__name__, template_folder=template_dir)
+
+  def test_table_view(self):
+    with self.app.test_request_context():
       columns = ['name', 'price']
-      tv = TableView(columns)
+      view = TableView(columns)
 
       model1 = Model(name="Renault Megane", price=10000)
       model2 = Model(name="Peugeot 308", price=12000)
 
       models = [model1, model2]
 
-      res = tv.render(models)
+      res = view.render(models)
+
+      assert "Renault Megane" in res
+      assert "10000" in res
+
+  def test_single_view(self):
+    with self.app.test_request_context():
+      panels = [Panel('main', Row('name', 'price'))]
+      view = SingleView(*panels)
+
+      model = Model(name="Renault Megane", price=10000)
+      res = view.render(model)
 
       assert "Renault Megane" in res
       assert "10000" in res
--- yaka/__init__.py
+++ yaka/__init__.py
@@ -1,1 +1,2 @@
 #noinspection PyUnresolvedReferences
+
