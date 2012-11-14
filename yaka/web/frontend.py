9fec3f15f812d918112bbf34022c805d68a77e65
Stefane Fermigier <sf@fermigier.com>
2012-11-14 14:59:09 +0100

    Extract widgets from web app.

--- yaka/web/frontend.py
+++ yaka/web/frontend.py
@@ -1,19 +1,19 @@
-import cgi
 import copy
-import json
 import re
 
 from flask import session, redirect, request, g, render_template, flash,\
-  Markup, Blueprint
+  Blueprint
 
 from yaka.web.decorators import templated
 from yaka.core.extensions import db
-from yaka.core.entities import Entity
 
 
 #
 # Helper classes
 #
+from yaka.web.widgets import Panel, Row, SingleView, TableView
+
+
 class BreadCrumbs(object):
   def __init__(self, l=()):
     self._bc = []
@@ -55,134 +55,6 @@ def add_to_recent_items(entity, type=None):
     del l[10:]
   session['recent_items'] = g.recent_items = l
 
-
-#
-# UI views
-#
-class TableView(object):
-  def __init__(self, columns, show_controls=False):
-    self.init_columns(columns)
-    self.name = id(self)
-    self.show_controls = show_controls
-
-  def init_columns(self, columns):
-    # TODO
-    self.columns = []
-    default_width = 0.99 / len(columns)
-    for col in columns:
-      if type(col) == str:
-        col = dict(name=col, width=default_width)
-      assert type(col) == dict
-      if not col.has_key('label'):
-        col['label'] = labelize(col['name'])
-      self.columns.append(col)
-
-  def render(self, model):
-    table = []
-    for entity in model:
-      table.append(self.render_line(entity))
-
-    return Markup(render_template('crm/render_table.html',
-                                  table=table, show_controls=self.show_controls,
-                                  columns=self.columns, table_name=self.name))
-
-  def render_line(self, entity):
-    line = []
-    for col in self.columns:
-      if type(col) == str:
-        column_name = col
-      else:
-        column_name = col['name']
-      value = getattr(entity, column_name)
-      if value is None:
-        value = ""
-      if column_name == '_name':
-        cell = Markup('<a href="%s">%s</a>' \
-                      % (entity._url, cgi.escape(value)))
-      elif isinstance(value, Entity):
-        cell = Markup('<a href="%s">%s</a>' \
-                      % (value._url, cgi.escape(value._name)))
-      elif (isinstance(value, str) or isinstance(value, unicode))\
-          and value.startswith("http://"):
-        # XXX: security issue here
-        cell = Markup('<a href="%s">%s</a>' % (value, value[len("http://"):]))
-      else:
-        cell = unicode(value)
-      line.append(cell)
-    return line
-
-
-class SingleView(object):
-  def __init__(self, *panels):
-    self.panels = panels
-
-  def render(self, model):
-    # TODO: refactor by passing a model instead
-    def get(attr_name):
-      return self.get(model, attr_name)
-
-    return Markup(render_template('crm/render_single.html', panels=self.panels, get=get))
-
-  def render_form(self, form, for_new=False):
-    # Client-side rules for jQuery.validate
-    # See: http://docs.jquery.com/Plugins/Validation/Methods#Validator
-    rules = {}
-    for field in form:
-      rules_for_field = {}
-      for validator in field.validators:
-        rule_for_validator = getattr(validator, "rule", None)
-        if rule_for_validator:
-          rules_for_field.update(rule_for_validator)
-      if rules_for_field:
-        rules[field.name] = rules_for_field
-    if rules:
-      rules = Markup(json.dumps(rules))
-    else:
-      rules = None
-
-    return Markup(render_template('crm/render_for_edit.html',
-                                  form=form, for_new=for_new, rules=rules))
-
-  def get(self, model, attr_name):
-    value = getattr(model, attr_name)
-    if value is None:
-      return ""
-    elif isinstance(value, Entity):
-      return Markup('<a href="%s">%s</a>' % (value._url, cgi.escape(value._name)))
-    else:
-      return unicode(value)
-
-
-#
-# Used to describe single entity views.
-#
-class Panel(object):
-  def __init__(self, label=None, *rows):
-    self.label = label
-    self.rows = rows
-
-  def __iter__(self):
-    return iter(self.rows)
-
-  def __getitem__(self, item):
-    return self.rows[item]
-
-  def __len__(self):
-    return len(self.rows)
-
-
-class Row(object):
-  def __init__(self, *cols):
-    self.cols = cols
-
-  def __iter__(self):
-    return iter(self.cols)
-
-  def __getitem__(self, item):
-    return self.cols[item]
-
-  def __len__(self):
-    return len(self.cols)
 
 
 def expose(url='/', methods=('GET',)):
