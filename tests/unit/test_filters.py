a5858b14a585c4e1e8b62fb97ae530ad7a27266e
Stefane Fermigier <sf@fermigier.com>
2012-12-06 10:59:10 +0100

    Fix failing test.

--- tests/unit/test_filters.py
+++ tests/unit/test_filters.py
@@ -8,10 +8,10 @@ from yaka.web.filters import filesize, date_age
 class TestFilters(unittest.TestCase):
 
   def test_filesize(self):
-    eq_("100 B", filesize(100))
-    eq_("1.0 kB", filesize(1000))
-    eq_("1.1 kB", filesize(1100))
-    eq_("10 kB", filesize(10000))
+    eq_("100&nbsp;B", str(filesize(100)))
+    eq_("1.0&nbsp;kB", str(filesize(1000)))
+    eq_("1.1&nbsp;kB", str(filesize(1100)))
+    eq_("10&nbsp;kB", str(filesize(10000)))
 
   def test_date_age(self):
     now = datetime.datetime(2012, 6, 10, 10, 10, 10)
