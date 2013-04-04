4765390537713d09b683c3cdbe22d128e00662f2
Stefane Fermigier <sf@fermigier.com>
2013-04-04 15:50:54 +0200

    Add integration tests for CRM app.

--- tests/integration/test_frontend.py
+++ tests/integration/test_frontend.py
@@ -1,12 +1,58 @@
 """
 Test the frontend.
 """
+from flask.ext.wtf import Form
+from sqlalchemy import Column, UnicodeText, String
+from abilian.core.entities import Entity
+from abilian.web.frontend import CRUDApp, Module
 
-from base import IntegrationTestCase
-from abilian.web.frontend import CRUDApp, BreadCrumbs
+from .base import IntegrationTestCase
+
+
+class EmailAddress(object):
+  pass
+
+
+class Contact(Entity):
+  __tablename__ = 'contact'
+
+  email = Column(String, nullable=False)
+  first_name = Column(UnicodeText)
+  last_name = Column(UnicodeText)
+
+
+class ContactEditForm(Form):
+  _groups = [
+    [u'Main', ['email', 'first_name', 'last_name']]
+  ]
+
+
+class Contacts(Module):
+  managed_class = Contact
+
+  list_view_columns = [
+    dict(name='_name', width=35),
+    dict(name='partenaire', width=25),
+    dict(name='titre', width=14),
+    dict(name='email', width=20)]
+
+  edit_form_class = ContactEditForm
+
+  related_views = [
+    # TODO
+    #('Visites', 'visites', ('partenaire', 'visiteur', 'date')),
+  ]
+
+
+class SimpleCRM(CRUDApp):
+  modules = [Contacts()]
+  url = "/crm"
 
 
 class FrontendTestCase(IntegrationTestCase):
+
+  def setUp(self):
+    crm = SimpleCRM(self.app)
 
   def test(self):
     pass
