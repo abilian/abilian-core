1ec5797d5cbf5627b30005faef678452956de2fa
Bertrand Mathieu <bmathieu@abilian.com>
2013-07-17 14:42:52 +0200

    util pagination: added "prev" "next" properties

--- abilian/core/tests/test_utils.py
+++ abilian/core/tests/test_utils.py
@@ -10,21 +10,37 @@ class TestPagination(TestCase):
     p = Pagination(1, 10, 10)
     l = list(p.iter_pages())
     self.assertEquals([1], l)
+    assert not p.has_prev
+    assert not p.has_next
+    assert p.prev is None
+    assert p.next is None
 
   def test2(self):
     p = Pagination(1, 10, 20)
     l = list(p.iter_pages())
     self.assertEquals([1, 2], l)
+    assert not p.has_prev
+    assert p.has_next
+    assert p.prev is None
+    assert p.next == 2
 
   def test3(self):
     p = Pagination(1, 10, 100)
     l = list(p.iter_pages())
     self.assertEquals([1, 2, 3, 4, 5, None, 9, 10], l)
+    assert not p.has_prev
+    assert p.has_next
+    assert p.prev is None
+    assert p.next == 2
 
   def test4(self):
     p = Pagination(10, 10, 100)
     l = list(p.iter_pages())
     self.assertEquals([1, 2, None, 8, 9, 10], l)
+    assert p.has_prev
+    assert not p.has_next
+    assert p.prev == 9
+    assert p.next is None
 
 
 class TestSlugify(TestCase):
--- abilian/core/util.py
+++ abilian/core/util.py
@@ -156,17 +156,25 @@ class Pagination(object):
     return self.page > 1
 
   @property
+  def prev(self):
+    return self.page - 1 if self.has_prev else None
+
+  @property
   def has_next(self):
     return self.page < self.pages
+
+  @property
+  def next(self):
+    return self.page + 1 if self.has_next else None
 
   def iter_pages(self, left_edge=2, left_current=2,
                  right_current=5, right_edge=2):
     last = 0
     for num in xrange(1, self.pages + 1):
-      if num <= left_edge or \
+      if (num <= left_edge or
           (num > self.page - left_current - 1
-              and num < self.page + right_current) or \
-          num > self.pages - right_edge:
+           and num < self.page + right_current) or
+          num > self.pages - right_edge):
         if last + 1 != num:
           yield None
         yield num
