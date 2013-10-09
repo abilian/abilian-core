960851f4d2c8ac62b7b185029076565b8459a026
Bertrand Mathieu <bmathieu@abilian.com>
2013-10-09 12:42:29 +0200

    fix testcase: use abilian.testing

--- tests/integration/base.py
+++ tests/integration/base.py
@@ -6,42 +6,16 @@ Base TestCase for integration tests.
 import fix_path
 
 import os
-import uuid
-import shutil
 
-from abilian.core.extensions import db
-from abilian.app import Application
 from abilian.testing import BaseTestCase
 
 from .config import TestConfig
 
-
 BASEDIR = os.path.dirname(__file__)
 
-
 class IntegrationTestCase(BaseTestCase):
-
+  config_class = TestConfig
   no_login = False
-
-  def create_app(self):
-    config = TestConfig()
-    config.WHOOSH_BASE = os.path.join(BASEDIR, "whoosh", str(uuid.uuid4()))
-    config.NO_LOGIN = self.no_login
-    self.app = Application(config=config)
-
-    return self.app
-
-  def setUp(self):
-    self.app.create_db()
-    self.session = db.session
-
-  def tearDown(self):
-    db.session.remove()
-    db.drop_all()
-    # clear whoosh
-    whoosh_base = self.app.config['WHOOSH_BASE']
-    if os.path.isdir(whoosh_base):
-      shutil.rmtree(whoosh_base)
 
   def assert_302(self, response):
     self.assert_status(response, 302)
--- tests/integration/config.py
+++ tests/integration/config.py
@@ -1,23 +1,12 @@
 # -*- coding: utf-8 -*-
 import os
 
+from abilian.testing import TestConfig as BaseConfig
 
-class TestConfig(object):
-  DEBUG = False
-  TESTING = True
-
-  SQLALCHEMY_DATABASE_URI = "sqlite://"
-  SQLALCHEMY_ECHO = False
+class TestConfig(BaseConfig):
 
   CELERY_ALWAYS_EAGER = True # run tasks locally, no async
   CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
 
-  CSRF_ENABLED = False
-  SECRET_KEY = "tototiti"
   SALT = "retwis"
   WHOOSH_BASE = "whoosh"
-
-  def __init__(self):
-    db_uri = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    if db_uri:
-      self.SQLALCHEMY_DATABASE_URI = db_uri
