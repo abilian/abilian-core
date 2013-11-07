5f72c3bc0e67492d207d89b5b7b761dd76f9cdd2
Bertrand Mathieu <bmathieu@abilian.com>
2013-11-07 11:39:26 +0100

    added manager command set "config"
     * "show" will show current app configuration
     * "init" will create a default config.py and logging.yml file in instance folder

---
+++ abilian/core/commands/base.py
@@ -0,0 +1,109 @@
+# coding=utf-8
+from __future__ import absolute_import
+import os
+import logging
+from pprint import pformat
+
+from flask import current_app
+from flask.ext.script import Manager
+from abilian.core.extensions import db
+from abilian.core.subjects import User
+from abilian.services import get_service
+
+__all__ = ['manager', 'logger']
+
+# Setup basic logging capabilities in case logging is not yet set up. From doc:
+# This function does nothing if the root logger already has handlers configured
+# for it.
+# Allow "print" statements to be replaced by a logging statements
+logging.basicConfig()
+logger = logging.getLogger('')
+
+#: ``flask.ext.script.Manager`` instance for abilian commands
+manager = Manager(usage='Abilian base commands')
+
+def print_config(config):
+  lines = ["Application configuration:"]
+  db_settings = set(current_app.services['settings'].namespace('config').keys())
+
+  for k, v in sorted(config.items()):
+    prefix = '    '
+    if k in db_settings:
+      prefix = '  * '
+    indent = len(k) + 3
+    width = 80 - indent
+    v = pformat(v, width=width).replace('\n', '\n'+ ' '*indent)
+    lines.append("{}{}: {}".format(prefix, k, v))
+  logger.info('\n'.join(lines))
+
+  from abilian.services import conversion
+  unoconv = conversion._unoconv_handler.unoconv
+  logger.info(
+    "Unoconv: {configured_path} ({abspath})\n"
+    "Version: {version}".format(
+      configured_path=unoconv,
+      abspath=os.path.abspath(unoconv),
+      version=conversion._unoconv_handler.unoconv_version))
+
+
+@manager.command
+def run():
+  """ Likes runserver, also print application configuration.
+  """
+  app = current_app
+  print_config(app.config)
+
+  DEBUG = app.config.get('DEBUG')
+  PORT = app.config.get('PORT', 5000)
+  app.run(host="0.0.0.0", debug=DEBUG, port=PORT)
+
+
+@manager.command
+def initdb():
+  """ Creates application DB.
+  """
+  current_app.create_db()
+
+
+@manager.command
+def dropdb():
+  """ Drops the application DB.
+  """
+  confirm = raw_input("Are you sure you want to drop the database? (Y/N) ")
+  if confirm.lower() == 'y':
+    with current_app.app_context():
+      current_app.db.drop_all()
+
+
+@manager.command
+def dumproutes():
+  """ Dumps all the routes registered in Flask.
+  """
+  rules = list(current_app.url_map.iter_rules())
+  rules.sort(key=lambda x: x.rule)
+  for rule in rules:
+    print "{} ({}) -> {}".format(rule, " ".join(rule.methods), rule.endpoint)
+
+
+@manager.command
+def createadmin(email, password, name=None, first_name=None):
+  """ Adds an admin user with given email and password.
+  """
+  user = User(email=email, password=password,
+              last_name=name, first_name=first_name,
+              can_login=True)
+  security = get_service('security')
+  security.grant_role(user, "admin")
+  db.session.add(user)
+  db.session.commit()
+  print "User {} added".format(email)
+
+
+@manager.command
+def changepassword(email, password):
+  """ Changes the password of the given user.
+  """
+  user = User.query.filter(User.email==email).one()
+  user.password = password
+  db.session.commit()
+  print "Password updated for user {}".format(email)
---
+++ abilian/core/commands/config.py
@@ -0,0 +1,115 @@
+# coding=utf-8
+"""
+"""
+from __future__ import absolute_import
+
+import os
+import sys
+
+from jinja2 import Environment, PackageLoader, Markup
+from flask import current_app
+from flask.ext.script import Manager
+
+from .base import print_config, logger
+#: sub-manager for config commands
+manager = Manager()
+
+@manager.command
+def show(only_path=False):
+  """ Show current config
+  """
+  app = current_app
+  infos = ['\n']
+  infos.append('Instance path: "{}"'.format(app.instance_path))
+  infos.append('CONFIG_ENVVAR: "{}"'.format(app.CONFIG_ENVVAR))
+
+  logger.info('\n  '.join(infos))
+
+  if not only_path:
+    print_config(app.config)
+
+
+class DefaultConfig(object):
+  SQLALCHEMY_DATABASE_URI = ''
+
+  PRODUCTION = False
+  LOGGING_CONFIG_FILE = 'logging.yml'
+
+  SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+  SQLALCHEMY_ECHO = False
+
+  DEBUG = True
+  ASSETS_DEBUG = True
+  DEBUG_TB_ENABLED = True
+  TEMPLATE_DEBUG = False
+  DEBUG_TB_INTERCEPT_REDIRECTS = False
+  DEBUG_TB_PROFILER_ENABLED = False
+
+  BROKER_URL = 'redis://localhost:6379/1'
+  CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
+
+  CELERYD_PREFETCH_MULTIPLIER = 1
+  CELERY_ALWAYS_EAGER = True # run tasks locally, no async
+  CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
+
+  WHOOSH_BASE = 'whoosh'
+
+  SITE_NAME = u''
+  MAIL_SENDER = u''
+
+  def __init__(self, logging_file=None):
+    self.SESSION_COOKIE_NAME = '{}-session'.format(current_app.name)
+    self.UNOCONV_LOCATION = os.path.join(sys.prefix, 'bin', 'unoconv')
+    self.SECRET_KEY = os.urandom(24)
+
+    if logging_file:
+      self.LOGGING_CONFIG_FILE = logging_file
+
+
+class ReprProxy(object):
+  """Proxy an object and apply repr() + Mark safe when accesing an
+  attribute. Used in jinja templates.
+  """
+  def __init__(self, obj):
+    self.__obj = obj
+
+  def __getattr__(self, name):
+    return Markup(repr(self.__obj.__getattribute__(name)))
+
+
+@manager.command
+def init(filename='config.py', logging_config='logging.yml'):
+  """
+  Creates a default config files in instance folder:
+
+  * [FILENAME] (default: "config.py")
+  * [LOGGING_CONFIG] (default: logging.yml
+
+  Defaults are tailored for development.
+  """
+  app = current_app
+  config_file = os.path.join(app.instance_path, filename)
+  logging_file = os.path.join(app.instance_path, logging_config)
+
+  if os.path.exists(config_file):
+    logger.info('Config file  "%s" already exists! Abort.', config_file)
+    return 1
+
+  config = DefaultConfig(logging_file=logging_config)
+  jinja_env = Environment(loader=PackageLoader(__name__, 'templates'))
+  template = jinja_env.get_template('config.py.jinja2')
+
+  with open(config_file, 'w') as f:
+    f.write(template.render(cfg=ReprProxy(config)))
+  logger.info('Generated "%s"', config_file)
+
+
+  if not os.path.exists(logging_file):
+    template = jinja_env.get_template('logging.yml.jinja2')
+    with open(logging_file, 'w') as f:
+      f.write(template.render())
+    logger.info('Generated "%s"', logging_file)
+  else:
+    logger.info('Logging config file "%s" already exists, skipping creation.',
+                logging_file)
+
