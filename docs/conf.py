db69162ce06c6eceb8a4b8cc74916798d3780d03
Bertrand Mathieu <bmathieu@abilian.com>
2013-11-22 12:00:57 +0100

    docs/exts is now an importable module

--- docs/conf.py
+++ docs/conf.py
@@ -16,9 +16,8 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
-sys.path.append(os.path.abspath('exts'))
 
 
 # -- General configuration -----------------------------------------------------
@@ -29,7 +28,7 @@ sys.path.append(os.path.abspath('exts'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    'intersphinx_flask_ext', # custom extension to convert 'flask_' to flask.ext.
+    'exts.intersphinx_flask_ext', # custom extension to convert 'flask_' to flask.ext.
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
