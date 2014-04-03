22a18e76a7390cf8e1b086d97da4285ab812d0f8
Bertrand Mathieu <bmathieu@abilian.com>
2014-04-03 16:18:43 +0200

    add project config for emacs: indent offset in python and config file to use with flycheck

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((python-mode
+  (py-indent-offset . 2)
+  (flycheck-pylintrc . "pylintrc")))
+
