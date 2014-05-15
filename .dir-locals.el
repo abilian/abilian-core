1f7051636e92100a00ab5b35abe5af49f010df78
Bertrand Mathieu <bmathieu@abilian.com>
2014-05-15 15:21:03 +0200

    fix files for emacs/pylint setup

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,6 +2,10 @@
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
 ((python-mode
+  (py-smart-indentation)
   (py-indent-offset . 2)
+  (flycheck-checker . python-pylint)
   (flycheck-pylintrc . "pylintrc")))
+
+
 
