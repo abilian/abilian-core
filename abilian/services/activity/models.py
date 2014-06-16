6b90f51bf710f0de803194163d1958663931e60b
Bertrand Mathieu <bmathieu@abilian.com>
2014-06-16 17:15:20 +0200

    * abilian/services/activity/models.py: fix TODO in module docstring

--- abilian/services/activity/models.py
+++ abilian/services/activity/models.py
@@ -5,8 +5,7 @@ See: http://activitystrea.ms/specs/json/1.0/
 See: http://activitystrea.ms/specs/atom/1.0/#activity
 See: http://stackoverflow.com/questions/1443960/how-to-implement-the-activity-stream-in-a-social-network
 
-TODO: replace `subject` by `target`. Also look wether other attributes from
-the spec need to be implemented.
+TODO: Look wether other attributes from the spec need to be implemented.
 """
 
 from datetime import datetime
