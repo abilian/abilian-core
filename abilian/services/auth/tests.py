abilian/abilian-core

commit 2e3a24e26771787dd77a65b7776555056638a5b3

PEP8 clean up.

abilian/services/auth/views.py
@@ -63,7 +63,7 @@ def login_post():
     return (render_template("login/login.html", next_url=next_url,), 401)
 
   try:
-    user = User.query.filter(User.email == email, User.can_login == True).one()
+    user = User.query.filter(User.email == email, User.can_login is True).one()
   except NoResultFound:
     flash(_(u"Sorry, we couldn't find an account for "
             "email '{email}'.").format(email=email), 'error')
@@ -84,6 +84,7 @@ def login_post():
 def logout():
   logout_user()
   return redirect(request.url_root)
+
 
 #
 # Code to deal with forgotten passwords or initial password generation
@@ -110,7 +111,7 @@ def forgotten_pw(new_user=False):
     return render_template("login/forgotten_password.html")
 
   try:
-    user = User.query.filter(User.email == email, User.can_login == True).one()
+    user = User.query.filter(User.email == email, User.can_login is True).one()
   except NoResultFound:
     flash(_(u"Sorry, we couldn't find an account for "
             "email '{email}'.").format(email=email), 'error')
@@ -298,13 +299,14 @@ def log_session_end(app, user):
 user_logged_in.connect(log_session_start)
 user_logged_out.connect(log_session_end)
 
+
 # login redirect utilities
 #  from http://flask.pocoo.org/snippets/62/
 def is_safe_url(target):
   ref_url = urlparse(request.host_url)
   test_url = urlparse(urljoin(request.host_url, target))
   return test_url.scheme in ('http', 'https') and \
-    ref_url.netloc == test_url.netloc
+      ref_url.netloc == test_url.netloc
 
 
 def get_redirect_target():

abilian/services/activity/tests.py
@@ -37,4 +37,3 @@ class ActivityTestCase(BaseTestCase):
     self.assertEquals(entry.object, message)
     self.assertEquals(entry.object_type, message.entity_type)
     self.assertEquals(entry.object_id, message.id)
-

abilian/services/audit/tests.py
@@ -2,10 +2,9 @@ import datetime
 from itertools import count
 
 import sqlalchemy as sa
-from sqlalchemy.orm.attributes import NO_VALUE, NEVER_SET
+from sqlalchemy.orm.attributes import NEVER_SET
 from sqlalchemy import (Column, Unicode, UnicodeText, Text, Date, ForeignKey,
-  Integer,
-)
+                        Integer)
 
 from abilian.core.entities import Entity, SEARCHABLE, AUDITABLE_HIDDEN
 from abilian.core.extensions import db
@@ -119,13 +118,13 @@ class TestAudit(BaseTestCase):
     entry = AuditEntry.query.order_by(AuditEntry.happened_at).all()[4]
     assert entry.type == DELETION
     assert entry.entity_id == account.id
-    assert entry.entity == None
+    assert entry.entity is None
 
     # check all entries are still present (but have lost reference to entity)
     entries = AuditEntry.query.all()
     assert len(entries) == 5
     assert all(e.entity_id == account.id for e in entries)
-    assert all(e.entity == None for e in entries)
+    assert all(e.entity is None for e in entries)
 
   def test_audit_related(self):
     AuditEntry.query.delete()
@@ -135,6 +134,7 @@ class TestAudit(BaseTestCase):
     #  helper
     audit_idx = count()
     audit_query = AuditEntry.query.order_by(AuditEntry.happened_at)
+
     def next_entry():
       return audit_query.all()[audit_idx.next()]
 
@@ -207,5 +207,3 @@ class TestAudit(BaseTestCase):
     assert entry.entity_id == account.id
     # entity not deleted: audit should still have reference to it
     assert entry.entity == account
-
-

abilian/services/preferences/tests.py
@@ -42,4 +42,3 @@ class PreferencesTestCase(BaseTestCase):
     self.session.flush()
     preferences = preference_service.get_preferences(user)
     self.assertEquals(preferences, {'some_int': 1, 'some_bool': True})
-

abilian/services/security/tests.py
@@ -11,7 +11,6 @@ from abilian.testing import BaseTestCase
 from . import security, RoleAssignment, InheritSecurity
 
 
-
 TEST_EMAIL = u"joe@example.com"
 TEST_PASSWORD = "tototiti"
 

abilian/services/settings/tests.py
@@ -8,6 +8,7 @@ from unittest import TestCase
 from abilian.testing import BaseTestCase
 from .models import Setting, EmptyValue
 
+
 class SettingTestCase(TestCase):
 
   def test_type_set(self):

abilian/web/admin/tests.py
@@ -39,4 +39,3 @@ class TestViews(BaseTestCase):
   def test_settings(self):
     response = self.client.get(url_for("admin.settings"))
     self.assert_200(response)
-

commit 987360209f9cc7512d839f7a3e0dba83aa669a13

* abilian/services/auth/views.py: fix query filter
bug introduced in 2e3a24e26771787dd77a65b7776555056638a5b3

abilian/services/auth/tests.py
@@ -48,3 +48,24 @@ class TestAuth(BaseTestCase):
 
     rv = self.client.post('/user/api/logout')
     self.assertEquals(rv.status_code, 200, "expected 200, got:" + rv.status)
+
+  def test_forgotten_pw(self):
+    mail = self.app.extensions['mail']
+    kwargs = dict(email=u'user@domain.tld', password='azerty', can_login=True)
+    u = User(**kwargs)
+    self.session.add(u)
+    self.session.commit()
+
+    payload = dict()
+    payload.update(kwargs)
+    del payload['password']
+
+    with mail.record_messages() as outbox:
+      rv = self.client.post('/user/forgotten_pw', data=kwargs)
+      self.assertEquals(rv.status_code, 302, "expected 302, got:" + rv.status)
+      self.assertEquals(len(outbox), 1)
+      msg = outbox[0]
+      self.assertEquals(msg.subject,
+                        u'Password reset instruction for Abilian Test')
+      self.assertEquals(msg.recipients, [u'user@domain.tld'])
+      self.assertEquals(msg.cc, [])

abilian/services/auth/views.py
@@ -150,7 +150,7 @@ def forgotten_pw(new_user=False):
     return render_template("login/forgotten_password.html")
 
   try:
-    user = User.query.filter(User.email == email, User.can_login is True).one()
+    user = User.query.filter(User.email == email, User.can_login == True).one()
   except NoResultFound:
     flash(_(u"Sorry, we couldn't find an account for "
             "email '{email}'.").format(email=email), 'error')