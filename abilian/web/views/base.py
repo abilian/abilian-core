8138228cce40ba4dbff99bf29b1ea406da1b7144
Bertrand Mathieu <bmathieu@abilian.com>
2014-07-31 16:53:14 +0200

    views: add JSONView

--- abilian/web/views/base.py
+++ abilian/web/views/base.py
@@ -3,7 +3,7 @@
 """
 from __future__ import absolute_import
 
-from flask import request, g
+from flask import request, g, json, jsonify, render_template_string
 from flask.views import MethodView as BaseView
 
 from ..action import actions
@@ -38,3 +38,53 @@ class View(BaseView):
     identifier by object in arguments.
     """
     return args, kwargs
+
+
+_JSON_HTML = u'''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="{{ url_for('abilian_static', filename="highlightjs/default.min.css") }}" />
+</head>
+<body>
+  <pre>
+  <code class="json">
+{{ content }}
+  </code>
+  </pre>
+
+  <script src="{{ url_for('abilian_static', filename="highlightjs/highlight.min.js")  }}" ></script>
+  <script>hljs.initHighlightingOnLoad();</script>
+</body>
+</html>
+'''
+
+
+class JSONView(View):
+  """
+  Base view for JSON GET.
+
+  Renders as JSON when requested by Ajax, renders as HTML when requested
+  from browser.
+  """
+  def prepare_args(self, args, kwargs):
+    kwargs.update({k:v for k,v in request.args.items()})
+    return args, kwargs
+
+  def data(self, *args, **kwargs):
+    """
+    This method should return data to be serialized using JSON
+    """
+    raise NotImplemented
+
+  def get(self, *args, **kwargs):
+    data = self.data(*args, **kwargs)
+    best_mime = request.accept_mimetypes.best_match(['text/html',
+                                                     'application/json'])
+    if best_mime == 'application/json':
+      return jsonify(data)
+
+    # dev requesting from browser? serve html, let debugtoolbar show up, etc...
+    content = json.dumps(data, indent=2)
+    return render_template_string(_JSON_HTML, content=content)
