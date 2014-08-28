5a0911e89a134063fdc68d80e0032db2545adfda
Bertrand Mathieu <bmathieu@abilian.com>
2014-08-28 12:36:46 +0200

    fix patching flask_mail

--- abilian/core/extensions.py
+++ abilian/core/extensions.py
@@ -10,6 +10,8 @@ Create all standard extensions.
 from __future__ import absolute_import
 
 __all__ = ['get_extension', 'db', 'mail', 'celery', 'login_manager', 'csrf']
+
+from .logging import patch_logger
 # celery
 #
 # for defining a task:
@@ -38,7 +40,49 @@ def _message_send(self, connection):
   self.extra_headers['Sender'] = sender
   connection.send(self, sender)
 
+patch_logger.info(flask_mail.Message.send)
 flask_mail.Message.send = _message_send
+
+# patch connection.send from flask-mail 0.9.0 with upstream code
+def _connection_send(self, message, envelope_from=None):
+  """Verifies and sends message.
+
+  :param message: Message instance.
+  :param envelope_from: Email address to be used in MAIL FROM command.
+  """
+  assert message.send_to, "No recipients have been added"
+
+  assert message.sender, (
+    "The message does not specify a sender and a default sender "
+    "has not been configured")
+
+  if message.has_bad_headers():
+    raise BadHeaderError
+
+  if message.date is None:
+    message.date = time.time()
+
+  if self.host:
+    self.host.sendmail(sanitize_address(envelope_from or message.sender),
+                       message.send_to,
+                       message.as_string(),
+                       message.mail_options,
+                       message.rcpt_options)
+
+  flask_mail.email_dispatched.send(
+    message,
+    app=current_app._get_current_object())
+
+  self.num_emails += 1
+
+  if self.num_emails == self.mail.max_emails:
+    self.num_emails = 0
+    if self.host:
+      self.host.quit()
+      self.host = self.configure_host()
+
+patch_logger.info(flask_mail.Connection.send)
+flask_mail.Connection.send = _connection_send
 
 mail = flask_mail.Mail()
 del flask_mail
