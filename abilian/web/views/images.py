f57617dac73a0a7bca9dd0d26df62541c0ce0626
Bertrand Mathieu <bmathieu@abilian.com>
2014-09-12 17:01:18 +0200

    user_avatar: ensure user's own avatar is always revalidated

--- abilian/web/views/images.py
+++ abilian/web/views/images.py
@@ -4,7 +4,11 @@ Blueprint for views of dynamic images
 """
 from __future__ import absolute_import
 
-from flask import Blueprint, request, abort, make_response
+import hashlib
+
+import sqlalchemy as sa
+
+from flask import Blueprint, request, abort, make_response, g
 from abilian.core.models.subjects import User, Group
 from abilian.services.image import crop_and_resize
 
@@ -31,17 +35,41 @@ def user_avatar(user_id):
   if size > 500:
     raise Exception("Error, size = %d" % size)
 
-  subject = User.query.get(user_id)
-  if subject is None:
+  user = User.query\
+    .options(sa.orm.undefer(User.photo))\
+    .get(user_id)
+
+  if user is None:
     abort(404)
 
-  photo = subject.photo
+  is_self = user.id == g.user.id
+
+  photo = user.photo
   if not photo:
     photo = open(DEFAULT_AVATAR, 'rb').read()
+
+  etag = None
+
+  if is_self:
+    # FIXME: there should be a photo_digest field on user object
+    acc = hashlib.md5(photo)
+    etag = acc.hexdigest()
+
+    if request.if_none_match and etag in request.if_none_match:
+      return Response(status=304)
+
 
   if size:
     photo = crop_and_resize(photo, size)
 
   response = make_response(photo)
   response.headers['content-type'] = 'image/jpeg'
+  if not is_self:
+    response.headers.add('Cache-Control', 'public, max-age=600')
+  else:
+    # current user's photo. It *must* be revalidated, so that it changes
+    # immediately after upload of a new one.
+    response.headers.add('Cache-Control', 'private, must-revalidate')
+    response.set_etag(etag)
+
   return response
