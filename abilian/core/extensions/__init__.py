a8578980c39c8aa32254b4c98d793af5071e002b
Bertrand Mathieu <bmathieu@abilian.com>
2014-09-29 10:55:21 +0200

    require flask-mail >= 0.9.1, remove patch needed when 0.9.0

--- abilian/core/extensions/__init__.py
+++ abilian/core/extensions/__init__.py
@@ -37,9 +37,15 @@ import flask.ext.mail as flask_mail
 
 # patch flask.ext.mail.Message.send to always set enveloppe_from default mail
 # sender
+# FIXME: we'ld rather subclass Message and update all imports
 def _message_send(self, connection):
+  """
+  Sends a single message instance. If TESTING is True the message will
+  not actually be sent.
+
+  :param message: a Message instance.
+  """
   sender = current_app.config['MAIL_SENDER']
-  enveloppe_from = current_app.config['MAIL_SENDER']
   if not self.extra_headers:
     self.extra_headers = {}
   self.extra_headers['Sender'] = sender
@@ -48,48 +54,6 @@ def _message_send(self, connection):
 patch_logger.info(flask_mail.Message.send)
 flask_mail.Message.send = _message_send
 
-# patch connection.send from flask-mail 0.9.0 with upstream code
-def _connection_send(self, message, envelope_from=None):
-  """Verifies and sends message.
-
-  :param message: Message instance.
-  :param envelope_from: Email address to be used in MAIL FROM command.
-  """
-  assert message.send_to, "No recipients have been added"
-
-  assert message.sender, (
-    "The message does not specify a sender and a default sender "
-    "has not been configured")
-
-  if message.has_bad_headers():
-    raise flask_mail.BadHeaderError
-
-  if message.date is None:
-    message.date = time.time()
-
-  if self.host:
-    self.host.sendmail(
-      flask_mail.sanitize_address(envelope_from or message.sender),
-      message.send_to,
-      message.as_string(),
-      getattr(message, 'mail_options', []),
-      getattr(message, 'rcpt_options', []))
-
-  flask_mail.email_dispatched.send(
-    message,
-    app=current_app._get_current_object())
-
-  self.num_emails += 1
-
-  if self.num_emails == self.mail.max_emails:
-    self.num_emails = 0
-    if self.host:
-      self.host.quit()
-      self.host = self.configure_host()
-
-
-patch_logger.info(flask_mail.Connection.send)
-flask_mail.Connection.send = _connection_send
 
 mail = flask_mail.Mail()
 
