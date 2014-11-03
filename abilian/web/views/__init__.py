637131914452b20b780a8895ec00bd43dff76104
Bertrand Mathieu <bmathieu@abilian.com>
2014-11-03 14:31:36 +0100

    views: export JSONView

--- abilian/web/views/__init__.py
+++ abilian/web/views/__init__.py
@@ -3,12 +3,12 @@
 """
 from __future__ import absolute_import
 
-from .base import View
+from .base import View, JSONView
 from .registry import Registry, default_view
 from .object import BaseObjectView, ObjectView, ObjectEdit, ObjectCreate, ObjectDelete
 
 __all__ = [
-    'View',
+    'View', 'JSONView'
     'Registry', 'default_view',
     'BaseObjectView', 'ObjectView', 'ObjectEdit', 'ObjectCreate', 'ObjectDelete',
     ]
