6593d076f46c19ac9b86eca1a26ff219ac8a9283
Bertrand Mathieu <bmathieu@abilian.com>
2015-01-14 12:12:24 +0100

    admin sysinfo panel: get package list from pkg_resources.working_set
    calling 'pip freeze' is not accurate when app is run from
    supervisor+gunicorn for example: /usr/bin/pip will be called, not virtualenv's pip

--- abilian/web/admin/panels/sysinfo.py
+++ abilian/web/admin/panels/sysinfo.py
@@ -4,8 +4,9 @@
 from __future__ import absolute_import
 
 import os
-import re
 import sys
+import pkg_resources
+from pip.vcs import vcs
 
 from flask import render_template
 
@@ -19,25 +20,28 @@ class SysinfoPanel(AdminPanel):
 
   def get(self):
     uname = os.popen("uname -a").read()
-
     python_version = sys.version.strip()
 
     lines = os.popen("pip freeze").readlines()
     packages = []
-    for line in lines:
-      line = line.strip()
-      if not line:
-        continue
-      m = re.search(r"egg=(\S+)$", line)
-      if m:
-        package = dict(name=m.group(1), version="from git")
-      else:
-        m = re.match(r"(\S+)==(.*)", line)
-        if m:
-          package = dict(name=m.group(1), version=m.group(2))
-        else:
-          package = dict(name=line, version="Unknown version")
+
+    for dist in pkg_resources.working_set:
+      package = dict(
+          name=dist.project_name,
+          key=dist.key,
+          version=dist.version if dist.has_version() else u'Unknown version',
+          vcs=None,
+      )
+
+      location = os.path.normcase(os.path.abspath(dist.location))
+      vcs_name = vcs.get_backend_name(location)
+      if vcs_name:
+        vc = vcs.get_backend_from_location(location)()
+        url, revision = vc.get_info(location)
+        package['vcs'] = dict(name=vcs_name, url=url, revision=revision)
+
       packages.append(package)
+      packages.sort(key=lambda d: d.get('key', None))
 
     return render_template("admin/sysinfo.html",
                            python_version=python_version,
