7aba6142a235f4f42e94ea55ffa6e93e72637cd4
Bertrand Mathieu <bmathieu@abilian.com>
2015-02-03 10:53:39 +0100

    form validator: add Length. Wtforms' one doesn't call gettext() properly

--- abilian/web/forms/validators.py
+++ abilian/web/forms/validators.py
@@ -11,7 +11,7 @@ from wtforms.validators import (
   Regexp, Email, IPAddress, MacAddress, URL, UUID, AnyOf, NoneOf
 )
 
-from abilian.i18n import _
+from abilian.i18n import _, _n
 from abilian.services import get_service
 
 
@@ -46,8 +46,46 @@ class EqualTo(EqualTo, Rule):
   pass
 
 
-class Length(Length, Rule):
-  pass
+class Length(Rule):
+  """
+  Validates the length of a string.
+
+  :param min: The minimum required length of the string. If not provided, minimum
+  length will not be checked.
+
+  :param max:  The maximum length of the string. If not provided, maximum length
+  will not be checked.
+
+  :param message: Error message to raise in case of a validation error. Can be
+  interpolated using `%(min)d` and `%(max)d` if desired. Useful defaults are
+  provided depending on the existence of min and max.
+  """
+  def __init__(self, min=-1, max=-1, message=None):
+    assert min != -1 or max != -1, 'At least one of `min` or `max` must be specified.'
+    assert max == -1 or min <= max, '`min` cannot be more than `max`.'
+    self.min = min
+    self.max = max
+    self.message = message
+
+  def __call__(self, form, field):
+    l = field.data and len(field.data) or 0
+
+    if l < self.min or self.max != -1 and l > self.max:
+      message = self.message
+      if message is None:
+        if self.max == -1:
+          message = _n(u'Field must be at least %(min)d character long.',
+                       u'Field must be at least %(min)d characters long.',
+                       self.min, min=self.min)
+        elif self.min == -1:
+          message = _n(u'Field cannot be longer than %(max)d character.',
+                       u'Field cannot be longer than %(max)d characters.',
+                       self.max, max=self.max)
+        else:
+          message = _(
+            u'Field must be between %(min)d and %(max)d characters long.',
+            min=self.min, max=self.max)
+      raise ValidationError(message % dict(min=self.min, max=self.max, length=l))
 
 
 class NumberRange(NumberRange, Rule):
