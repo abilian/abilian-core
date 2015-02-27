d4fc937d3494571b63469d99a49b91ae74b224e0
Bertrand Mathieu <bmathieu@abilian.com>
2015-02-27 15:24:10 +0100

    setup.py: setup_requires babel (for compile_catalog command)

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setuptools.setup(
   packages=['abilian'],
   zip_safe=False,
   platforms='any',
-  setup_requires=['setuptools-git', 'setuptools_scm'],
+  setup_requires=['babel', 'setuptools-git', 'setuptools_scm'],
   install_requires=install_requires,
   extras_require={
     'tests': dev_requires,
