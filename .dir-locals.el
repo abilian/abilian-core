4aa7f3f13a2d7c002b1f89565e3f5fd607be5c0f
Bertrand Mathieu <bmathieu@abilian.com>
2015-05-15 11:56:03 +0200

    .dir-locals.el: fix emacs config for project edit

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,11 +1,13 @@
 ;;; Directory Local Variables
-;;; See Info node `(emacs) Directory Variables' for more information.
+;;; For more information see (info "(emacs) Directory Variables")
 
 ((python-mode
+  (python-indent-offset . 2)
   (py-continuation-offset . 4)
   (py-smart-indentation)
   (py-indent-offset . 2)
   (flycheck-pylintrc . "pylintrc")))
+
 
 
 
