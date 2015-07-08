7e6042290fe8f36e51633da6efbfdafa64df7951
Bertrand Mathieu <bmathieu@abilian.com>
2015-07-08 17:23:48 +0200

    attachments: add download view

--- abilian/web/attachments/views.py
+++ abilian/web/attachments/views.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 
 import sqlalchemy as sa
 from werkzeug.exceptions import BadRequest
+from flask import send_file
 
 from abilian.i18n import _, _l
 from abilian.core.entities import Entity
@@ -12,7 +13,8 @@ from abilian.core.models.attachment import Attachment, is_support_attachments
 from abilian.web import url_for, nav
 from abilian.web.blueprints import Blueprint
 from abilian.web.action import actions, ButtonAction
-from abilian.web.views import ObjectCreate
+from abilian.web.views import BaseObjectView, ObjectCreate
+
 from .forms import AttachmentForm
 
 bp = Blueprint('attachments', __name__, url_prefix='/attachments')
@@ -30,46 +32,28 @@ UPLOAD_BUTTON = ButtonAction('form', 'edit', btn_class='primary',
                               title=_l(u'Send'))
 
 
-class AttachmentCreateView(ObjectCreate):
+class BaseAttachmentView(object):
   """
+  Mixin for attachment views
   """
   Model = Attachment
   Form = AttachmentForm
-  _message_success = _l(u"Attachment added")
 
-  #: attachmented entity
+  #: owning entity
   entity = None
 
-  def __init__(self, *args, **kwargs):
-    super(AttachmentCreateView, self).__init__(*args, **kwargs)
-
   def init_object(self, args, kwargs):
-    args, kwargs = super(AttachmentCreateView, self).init_object(args, kwargs)
+    args, kwargs = super(BaseAttachmentView, self).init_object(args, kwargs)
     entity_id = kwargs.pop('entity_id', None)
+
     if entity_id is not None:
       self.entity = Entity.query.get(entity_id)
 
     if self.entity is None:
       raise BadRequest('No entity provided')
 
-      if not is_support_attachments(self.entity):
-        raise BadRequest('This entity is doesn\'t support attachments')
-
-    self.obj.entity = self.entity
-    session = sa.orm.object_session(self.entity)
-
-    if session:
-      sa.orm.session.make_transient(self.obj)
-
     actions.context['object'] = self.entity
     return args, kwargs
-
-  def breadcrumb(self):
-    label = _(u'New attachment on "{title}"').format(title=self.entity.name)
-    return nav.BreadcrumbItem(label=label)
-
-  def get_form_buttons(self, *args, **kwargs):
-    return [UPLOAD_BUTTON]
 
   def view_url(self):
     kw = {}
@@ -83,6 +67,56 @@ class AttachmentCreateView(ObjectCreate):
   @property
   def activity_target(self):
     return self.entity
+
+
+class AttachmentDownload(BaseAttachmentView, BaseObjectView):
+  """
+  """
+  def get(self):
+    blob = self.obj.blob
+    metadata = blob.meta
+    filename = metadata.get('filename', self.obj.name)
+    content_type = metadata.get('mimetype', None)
+    stream = blob.file.open('rb')
+
+    return send_file(stream,
+                     as_attachment=True,
+                     attachment_filename=filename,
+                     mimetype=content_type,
+                     cache_timeout=0,
+                     add_etags=False,
+                     conditional=False)
+
+
+download_view = AttachmentDownload.as_view('download')
+bp.route('/<int:entity_id>/<int:object_id>/download')(download_view)
+
+
+class AttachmentCreateView(BaseAttachmentView, ObjectCreate):
+  """
+  """
+  _message_success = _l(u"Attachment added")
+
+  def init_object(self, args, kwargs):
+    args, kwargs = super(AttachmentCreateView, self).init_object(args, kwargs)
+
+    if not is_support_attachments(self.entity):
+      raise BadRequest('This entity is doesn\'t support attachments')
+
+    self.obj.entity = self.entity
+    session = sa.orm.object_session(self.entity)
+
+    if session:
+      sa.orm.session.make_transient(self.obj)
+
+    return args, kwargs
+
+  def breadcrumb(self):
+    label = _(u'New attachment on "{title}"').format(title=self.entity.name)
+    return nav.BreadcrumbItem(label=label)
+
+  def get_form_buttons(self, *args, **kwargs):
+    return [UPLOAD_BUTTON]
 
 
 create_view = AttachmentCreateView.as_view('create')
