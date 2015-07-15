f73208bb6d9846bc18bec79a79c47e19d4874458
Bertrand Mathieu <bmathieu@abilian.com>
2015-07-15 20:48:54 +0200

    fix TagsField: add/remove only self.ns tags
    tags with another namespace are preserved

--- abilian/web/tags/forms.py
+++ abilian/web/tags/forms.py
@@ -50,6 +50,10 @@ class TagsField(Field):
   def default(self):
     return set()
 
+  def process_data(self, data):
+    data = {t for t in data if t.ns == self.ns}
+    return super(TagsField, self).process_data(data)
+
   def process_formdata(self, valuelist):
     extension = current_app.extensions['tags']
     valuelist = set(valuelist[0].split(u';'))
@@ -65,8 +69,11 @@ class TagsField(Field):
 
   def populate_obj(self, obj, name):
     extension = current_app.extensions['tags']
+    # all_tags is an InstrumentedSet. add/remove will result in DB operations.
     all_tags = extension.entity_tags(obj)
-    to_remove = all_tags - self.data
+    all_ns_tags = {t for t in extension.entity_tags(obj)
+                if t.ns == self.ns}
+    to_remove = all_ns_tags - self.data
 
     for tag in to_remove:
       all_tags.remove(tag)
