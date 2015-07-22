77641075cc5efc62fdbb9fd4df5c7d3df7db8456
Bertrand Mathieu <bmathieu@abilian.com>
2015-07-22 17:02:24 +0200

    ravenjs: add console plugin

--- abilian/app.py
+++ abilian/app.py
@@ -124,7 +124,7 @@ default_config.update(
     SENTRY_USER_ATTRS=('email', 'first_name', 'last_name',),
     SENTRY_INSTALL_CLIENT_JS=True, # also install client JS
     SENTRY_JS_VERSION='1.1.19',
-    SENTRY_JS_PLUGINS=('jquery', 'native', 'require'),
+    SENTRY_JS_PLUGINS=('console', 'jquery', 'native', 'require',),
     SESSION_COOKIE_NAME=None,
     SQLALCHEMY_POOL_RECYCLE=1800, # 30min. default value in flask_sa is None
     SQLALCHEMY_TRACK_MODIFICATIONS=False,
