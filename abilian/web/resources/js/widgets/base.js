0483a802a713ef98ac326149d55e1c44a634ea17
Bertrand Mathieu <bmathieu@abilian.com>
2015-07-22 17:04:01 +0200

    initJsWidget: throw error with unknown widget name
    more useful error than 'widgetsCreators[name] is undefined'

--- abilian/web/resources/js/widgets/base.js
+++ abilian/web/resources/js/widgets/base.js
@@ -28,14 +28,20 @@
      * Initialiaze a single element.
      */
     Abilian.initJsWidget = function() {
-        var $this = $(this);
-        var creatorName = $this.data('init-with');
-        var params = $this.data('init-params');
+        var $this = $(this),
+            creatorName = $this.data('init-with'),
+            params = $this.data('init-params'),
+            creatorFunc = widgetsCreators[creatorName];
+
+        if (creatorFunc === undefined) {
+            throw new Error('Unknown widget constructor: "' + creatorName + '"');
+        }
 
         if (!(params instanceof Array)) {
             params = new Array(params);
         }
-        widgetsCreators[creatorName].apply($this, params);
+
+        creatorFunc.apply($this, params);
     };
 
     /*
