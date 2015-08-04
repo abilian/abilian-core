086216f57d1f9feb8c3d1a5b3b059486f1bc843e
Bertrand Mathieu <bmathieu@abilian.com>
2015-08-04 11:09:50 +0200

    image input: resize modes on client side

--- abilian/web/resources/js/widgets/image.js
+++ abilian/web/resources/js/widgets/image.js
@@ -8,9 +8,16 @@
     /**
      * Image input widget. Uses FileAPI (http://mailru.github.io/FileAPI/)
      */
+    var RESIZE_MODES = {
+        SCALE: 'scale',
+        FIT: 'fit',
+        CROP: 'crop'
+    };
+
     var defaults = {
         width: 55,
-        height: 55
+        height: 55,
+        resize_mode: 'FIT'
     };
 
     function ImageInput(node, options) {
@@ -19,23 +26,62 @@
         var $preview = $(this.preview_node);
         this.width = $preview.data('width') || options.width;
         this.height = $preview.data('height') || options.height;
-
+        this.resize_mode = RESIZE_MODES[options.resize_mode]
+            || RESIZE_MODES.FIT;
         // api.event.on(this.fileapi_node, 'change', this.changeImage.bind(this));
     }
 
     ImageInput.prototype = Object.create(FileInput.prototype);
 
     ImageInput.prototype.createFileNode = function(file) {
-        var el = FileInput.prototype.createFileNode.call(this, file),
-            image = api.Image(file),
-            preview = image.preview(this.width, this.height, 'max');
+        var self = this,
+            el = FileInput.prototype.createFileNode.call(self, file),
+            image = api.Image(file);
 
-        preview.get(function( err/**String*/, img/**HTMLElement*/ ) {
-            el.prepend(img);
+        api.getInfo(file, function(err, infos) {
+            if (err) {
+                return;
+            }
+
+            var resize_fun = self['resize_' + self.resize_mode],
+                preview;
+
+            preview = resize_fun.call(self, image, infos);
+            preview.get(function( err/**String*/, img/**HTMLElement*/ ) {
+                el.prepend(img);
+            });
         });
-
         return el;
     };
+
+    /* resize mode functions */
+    ImageInput.prototype.resize_scale = function(image, infos) {
+        return image.resize(this.width, this.height, 'max');
+    };
+
+
+    ImageInput.prototype.resize_fit = function(image, infos) {
+        var w_ratio = infos.width / this.width,
+            h_ratio = infos.height / this.height,
+            width, height, preview;
+
+        if (w_ratio > h_ratio) {
+            /* wider than taller: set target width to max width */
+            width = this.width;
+            height = Math.round(infos.height / w_ratio);
+        } else {
+            height = this.height;
+            width = Math.round(infos.width / h_ratio);
+        }
+
+        return image.resize(width, height, 'preview');
+    };
+
+
+    ImageInput.prototype.resize_crop = function(image, infos) {
+        return image.resize(this.width, this.height, 'preview');
+    };
+
 
     function createImageInput(options) {
         var element = $(this),
