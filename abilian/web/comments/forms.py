1b8e53ec50d0201aeedb85f007047fa25af52a15
Bertrand Mathieu <bmathieu@abilian.com>
2015-11-03 17:31:14 +0100

    change labels in comment form

--- abilian/web/comments/forms.py
+++ abilian/web/comments/forms.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import, print_function, division
 
 from wtforms.fields import TextAreaField
 
+from abilian.i18n import _l
 from abilian.core.models.comment import Comment
 from abilian.web.forms import Form
 from abilian.web.forms.widgets import TextArea
@@ -15,6 +16,7 @@ from abilian.web.forms.filters import strip
 class CommentForm(Form):
 
   body = TextAreaField(
+    label=_l(u'Comment'),
     validators=[required()],
     filters=(strip,),
     widget=TextArea(rows=5, resizeable='vertical'),
@@ -24,4 +26,3 @@ class CommentForm(Form):
     model = Comment
     include_primary_keys = True
     assign_required = False # for 'id': allow None, for new records
-
--- abilian/web/comments/views.py
+++ abilian/web/comments/views.py
@@ -27,7 +27,7 @@ def register_default_view(state):
   state.app.default_view.register(Comment, _default_comment_view)
 
 COMMENT_BUTTON = ButtonAction('form', 'edit', btn_class='primary',
-                              title=_l(u'Send'))
+                              title=_l(u'Post'))
 
 
 class CommentCreateView(ObjectCreate):
