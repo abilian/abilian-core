a7b54f717e12eac732805930ca3e580c3e26145b
Stefane Fermigier <sf@fermigier.com>
2017-01-10 00:01:06 +0100

    Revert "Update jquery and silence warnings."
    This reverts commit 0ed8927aa37e3bf51722f7c67298bec994a277f9.

--- abilian/web/assets/__init__.py
+++ abilian/web/assets/__init__.py
@@ -44,7 +44,8 @@ def requirejs_config():
 
 RESOURCES_DIR = pkg_resources.resource_filename('abilian.web', 'resources')
 
-JQUERY = Bundle('jquery/js/jquery-1.12.4.js')
+JQUERY = Bundle('jquery/js/jquery-1.11.3.js',
+                'jquery/js/jquery-migrate-1.2.1.js')
 
 BOOTBOX_JS = Bundle('bootbox/bootbox.js')
 
