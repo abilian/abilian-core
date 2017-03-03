a4d8c995448ade637c8659c84b6e184563df9a66
vindarel <ehvince@mailz.org>
2017-03-03 17:39:54 +0100

    Replace progressbar with tqdm

--- abilian/core/commands/indexing.py
+++ abilian/core/commands/indexing.py
@@ -7,7 +7,6 @@ from __future__ import absolute_import, division, print_function, \
 import time
 from collections import deque
 
-import progressbar
 import sqlalchemy as sa
 import whoosh
 from flask import current_app
