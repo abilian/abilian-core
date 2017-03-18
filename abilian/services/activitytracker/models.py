0ec9746dc37f2738b10cf45a23c2e40d5307ec2b
ahmedaminehamrouni <ahmed.amine.hamrouni.1@gmail.com>
2017-03-18 01:27:13 -0700

    Fixing signals

--- abilian/services/activitytracker/models.py
+++ abilian/services/activitytracker/models.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import, division, print_function, \
     unicode_literals
 
-from sqlalchemy import Column, DateTime, Integer
+from sqlalchemy import Column, DateTime, Integer, String
 
 from abilian.core.extensions import db
 from datetime import datetime
@@ -21,3 +21,17 @@ class Viewed(db.Model):
 
     #: time of view
     viewed_at = Column(DateTime, default=datetime.utcnow, nullable=True)
+
+
+class SignalLog(db.Model):
+    __tablename__ = "signal_log"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+
+    sender = Column(String(120), nullable=False)
+
+    object_id = Column(Integer, nullable=False)
+
+    user_id = Column(Integer, nullable=False)
+
+    signal_time = Column(DateTime, default=datetime.utcnow, nullable=True)
--- abilian/services/activitytracker/service.py
+++ abilian/services/activitytracker/service.py
@@ -7,57 +7,27 @@ from __future__ import absolute_import, division, print_function, \
 from abilian.services import Service
 from abilian.core.extensions import db
 from datetime import datetime
-from blinker import signal
-from .models import Viewed
-import logging
+from .models import Viewed, SignalLog
 
 
 class ActivityTracker(Service):
     name = 'activitytracker'
 
-    def __init__(self, has_signals=False):
-        self.has_signals = has_signals
-        if self.has_signals:
-            self.init_signals()
-
-    def init_app(self, app):
-        Service.init_app(self, app)
-
-    def init_signals(self):
-        self.traked_object_signal = signal('object-tracked')
-        self.get_traks_signal = signal('get-traks')
-        self.get_viewers_signal = signal('get-viewers')
-        self.traked_object_signal.connect(self.traker_signal_callback)
-        self.get_traks_signal.connect(self.traker_signal_callback)
-        self.get_viewers_signal.connect(self.traker_signal_callback)
-
-    def start(self):
-        Service.start(self)
-
-    def stop(self):
-        Service.stop(self)
-
-    def traker_signal_callback(self, sender, action):
-        logging.warn("{} : {} : {}".format(sender.name, action, datetime.utcnow()))
+    def signal_callback(self, sender, object_id, user_id):
+        db.session.add(SignalLog(sender=sender, object_id=object_id, user_id=user_id))
+        db.session.commit()
 
     def track_object(self, object_id, user_id):
-        if self.has_signals:
-            self.traked_object_signal.send(self, action="object-tracked")
         if not Viewed.query.filter(Viewed.object_id == object_id, Viewed.user_id == user_id).count():
             db.session.add(Viewed(object_id=object_id, user_id=user_id))
             db.session.commit()
         else:
-            Viewed.query.filter(Viewed.object_id == object_id, Viewed.user_id == user_id)\
-            .update({Viewed.viewed_at: datetime.utcnow()})
+            Viewed.query.filter(Viewed.object_id == object_id, Viewed.user_id == user_id).update({Viewed.viewed_at: datetime.utcnow()})
 
     def get_tracked_object(self, object_id, user_id):
-        if self.has_signals:
-            self.get_traks_signal.send(self, action="get-tracks")
         track = Viewed.query.filter(Viewed.object_id == object_id, Viewed.user_id == user_id).first()
         return track
 
     def get_viewers(self, object_id):
-        if self.has_signals:
-            self.get_viewers_signal.send(self, action="get-viewers")
         viewers = Viewed.query.filter(Viewed.object_id == object_id).all()
         return viewers
