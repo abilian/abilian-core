5112e831149b72dcac36693ec6c8dab0685b2cb9
ahmedaminehamrouni <ahmed.amine.hamrouni.1@gmail.com>
2017-03-25 01:05:58 -0700

    updating activitytracker service

--- abilian/services/activitytracker/service.py
+++ abilian/services/activitytracker/service.py
@@ -22,13 +22,20 @@ class ActivityTracker(Service):
         else:
             Viewed.query.filter(Viewed.object_id == object_id, Viewed.user_id == user_id).update({Viewed.viewed_at: datetime.utcnow()})
 
+    # get a specific tracked object
     def get_tracked_object(self, object_id, user_id):
         track = Viewed.query.filter(Viewed.object_id == object_id, Viewed.user_id == user_id).first()
         return track
 
+    # get all viewers of a specific object
     def get_viewers(self, object_id):
         viewers = Viewed.query.filter(Viewed.object_id == object_id).all()
         return viewers
+
+    # get all viewed objects from a specific user
+    def get_viewed_objects(self, user_id):
+        viewed_objects = Viewed.query.filter(Viewed.user_id == user_id).all()
+        return viewed_objects
 
 
 # Instanciate the service
