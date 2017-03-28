5e7bfd22c32d7bb4379b04c3564d097a26e8eb1f
ahmedaminehamrouni <ahmed.amine.hamrouni.1@gmail.com>
2017-03-27 20:08:15 -0700

    Updating service + adding object tracks

--- abilian/services/activitytracker/service.py
+++ abilian/services/activitytracker/service.py
@@ -4,11 +4,8 @@
 from __future__ import absolute_import, division, print_function, \
     unicode_literals
 
-from datetime import datetime
-
-from abilian.core.extensions import db
 from abilian.services import Service
-
+from abilian.core.extensions import db
 from .models import Viewed
 
 __all__ = ['activitytracker', 'ActivityTracker']
@@ -18,30 +15,38 @@ class ActivityTracker(Service):
     name = 'activitytracker'
 
     def track_object(self, object_id, user_id):
-        if not Viewed.query.filter(Viewed.object_id == object_id,
-                                   Viewed.user_id == user_id).count():
-            db.session.add(Viewed(object_id=object_id, user_id=user_id))
-            db.session.commit()
-        else:
-            Viewed.query.filter(Viewed.object_id == object_id,
-                                Viewed.user_id == user_id).update({
-                                    Viewed.viewed_at: datetime.utcnow()
-                                })
+        db.session.add(Viewed(object_id=object_id, user_id=user_id))
+        db.session.commit()
 
     # get a specific tracked object
     def get_tracked_object(self, object_id, user_id):
-        track = Viewed.query.filter(Viewed.object_id == object_id,
-                                    Viewed.user_id == user_id).first()
+        track = Viewed.query \
+            .filter(Viewed.object_id == object_id, Viewed.user_id == user_id) \
+            .order_by(Viewed.viewed_at.asc()) \
+            .all()[-1]
         return track
+
+    #: get all tracks that are related to a specific object
+    def get_object_tracks(self, object_id):
+        object_tracks = Viewed.query \
+            .filter(Viewed.object_id == object_id) \
+            .all()
+        return object_tracks
 
     # get all viewers of a specific object
     def get_viewers(self, object_id):
-        viewers = Viewed.query.filter(Viewed.object_id == object_id).all()
+        viewers = Viewed.query \
+            .filter(Viewed.object_id == object_id) \
+            .group_by(Viewed.user_id) \
+            .all()
         return viewers
 
     # get all viewed objects from a specific user
     def get_viewed_objects(self, user_id):
-        viewed_objects = Viewed.query.filter(Viewed.user_id == user_id).all()
+        viewed_objects = Viewed.query \
+            .filter(Viewed.user_id == user_id) \
+            .group_by(Viewed.object_id) \
+            .all()
         return viewed_objects
 
 
