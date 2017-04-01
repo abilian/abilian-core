f5be09ba98c4e79f5f0c9ff2b16d8913fbc3c794
ahmedaminehamrouni <ahmed.amine.hamrouni.1@gmail.com>
2017-03-31 21:15:17 -0700

    updating service condition

--- abilian/services/activitytracker/service.py
+++ abilian/services/activitytracker/service.py
@@ -15,38 +15,43 @@ class ActivityTracker(Service):
     name = 'activitytracker'
 
     def track_object(self, object_id, user_id):
-        db.session.add(ViewsTrack(object_id=object_id, user_id=user_id))
-        db.session.commit()
+        if object_id and user_id:
+            db.session.add(ViewsTrack(object_id=object_id, user_id=user_id))
+            db.session.commit()
 
     #: get tracks of a specific object and user
     def get_tracked_object(self, object_id, user_id):
-        view_objects = ViewsTrack.query \
-            .filter(ViewsTrack.object_id == object_id, ViewsTrack.user_id == user_id) \
-            .order_by(ViewsTrack.viewed_at.asc()) \
-            .all()
+        if object_id and user_id:
+            view_objects = ViewsTrack.query \
+                .filter(ViewsTrack.object_id == object_id, ViewsTrack.user_id == user_id) \
+                .order_by(ViewsTrack.viewed_at.asc()) \
+                .all()
         return view_objects
 
     #: get all tracks that are related to a specific object
     def get_object_tracks(self, object_id):
-        view_objects = ViewsTrack.query \
-            .filter(ViewsTrack.object_id == object_id) \
-            .all()
+        if object_id:
+            view_objects = ViewsTrack.query \
+                .filter(ViewsTrack.object_id == object_id) \
+                .all()
         return view_objects
 
     # get all viewers of a specific object
     def get_viewers(self, object_id):
-        view_objects = ViewsTrack.query \
-            .filter(ViewsTrack.object_id == object_id) \
-            .group_by(ViewsTrack.user_id) \
-            .all()
+        if object_id:
+            view_objects = ViewsTrack.query \
+                .filter(ViewsTrack.object_id == object_id) \
+                .group_by(ViewsTrack.user_id) \
+                .all()
         return view_objects
 
     # get all ViewTrack objects from a specific user
     def get_viewed_objects(self, user_id):
-        view_objects = ViewsTrack.query \
-            .filter(ViewsTrack.user_id == user_id) \
-            .group_by(ViewsTrack.object_id) \
-            .all()
+        if user_id:
+            view_objects = ViewsTrack.query \
+                .filter(ViewsTrack.user_id == user_id) \
+                .group_by(ViewsTrack.object_id) \
+                .all()
         return view_objects
 
 
