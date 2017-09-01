ff4577abfbbc3aa19a8092b9fa14d7411aa99b3c
Stefane Fermigier <sf@fermigier.com>
2017-09-01 13:59:31 +0200

    Remove deprecated decorator.

--- abilian/web/decorators.py
+++ abilian/web/decorators.py
@@ -8,37 +8,6 @@ from __future__ import absolute_import, division, print_function, \
 import warnings
 from functools import wraps
 
-from flask import render_template, request
-
-__all__ = ['templated']
-
-
-# Copy/pasted from: http://flask.pocoo.org/docs/patterns/viewdecorators/
-def templated(template=None):
-    """The idea of this decorator is that you return a dictionary with the values
-    passed to the template from the view function and the template is automatically rendered.
-
-    @deprecated
-    """
-
-    def decorator(f):
-
-        @wraps(f)
-        def decorated_function(*args, **kwargs):
-            template_name = template
-            if template_name is None:
-                template_name = request.endpoint.replace('.', '/') + '.html'
-            ctx = f(*args, **kwargs)
-            if ctx is None:
-                ctx = {}
-            elif not isinstance(ctx, dict):
-                return ctx
-            return render_template(template_name, **ctx)
-
-        return decorated_function
-
-    return decorator
-
 
 # Copy/pasted from:
 # https://wiki.python.org/moin/PythonDecoratorLibrary#Generating_Deprecation_Warnings
