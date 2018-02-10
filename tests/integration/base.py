7e68e5d071bbd78438624a45a51336d9e13c462e
Stefane Fermigier <sf@fermigier.com>
2018-02-10 14:23:43 +0100

    refactor: remove buggy / unneeded code.

--- tests/integration/base.py
+++ tests/integration/base.py
@@ -4,21 +4,11 @@ Base TestCase for integration tests.
 """
 from __future__ import absolute_import, print_function, unicode_literals
 
-import os
-
 from abilian.testing import BaseTestCase
 
 from .config import TestConfig
-
-BASEDIR = os.path.dirname(__file__)
 
 
 class IntegrationTestCase(BaseTestCase):
     config_class = TestConfig
     no_login = False
-
-    def assert_302(self, response):
-        assert response.status_core == 302
-
-    def assert_204(self, response):
-        assert response.status_core == 204
