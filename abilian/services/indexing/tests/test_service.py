35c3868cab59c9c92c0fc360bf45cc9e48add903
Stefane Fermigier <sf@fermigier.com>
2018-02-15 17:38:20 +0100

    refactor one last test.

--- abilian/services/indexing/tests/test_service.py
+++ abilian/services/indexing/tests/test_service.py
@@ -4,9 +4,9 @@ from __future__ import absolute_import, division, print_function, \
     unicode_literals
 
 import sqlalchemy as sa
+from pytest import yield_fixture
 
 from abilian.core.entities import Entity
-from abilian.testing import BaseTestCase
 
 
 class IndexedContact(Entity):
@@ -15,49 +15,48 @@ class IndexedContact(Entity):
     name = sa.Column(sa.UnicodeText)
 
 
-class IndexingServiceTestCase(BaseTestCase):
-    SERVICES = ('indexing',)
+@yield_fixture
+def svc(app):
+    svc = app.services['indexing']
+    with app.app_context():
+        svc.start()
+        yield svc
 
-    def setUp(self):
-        BaseTestCase.setUp(self)
-        self.svc = self.app.services['indexing']
 
-    def get_setup_config(self):
-        cfg = BaseTestCase.get_setup_config(self)
-        # cfg.SQLALCHEMY_ECHO = True
-        return cfg
+def test_app_state(app, svc):
+    state = svc.app_state
+    assert IndexedContact in state.indexed_classes
+    assert IndexedContact.entity_type in svc.adapted
+    assert IndexedContact.entity_type in state.indexed_fqcn
 
-    def test_app_state(self):
-        state = self.svc.app_state
-        assert IndexedContact in state.indexed_classes
-        assert IndexedContact.entity_type in self.svc.adapted
-        assert IndexedContact.entity_type in state.indexed_fqcn
 
-    def test_index_only_after_final_commit(self):
-        contact = IndexedContact(name='John Doe')
-        state = self.svc.app_state
-        self.session.begin(nested=True)
+def test_index_only_after_final_commit(app, session, svc):
+    contact = IndexedContact(name='John Doe')
 
-        assert state.to_update == []
-        self.session.add(contact)
+    state = svc.app_state
 
-        # no commit: model is in wait queue
-        self.session.flush()
-        assert state.to_update == [('new', contact)]
+    session.begin(nested=True)
+    assert state.to_update == []
 
-        # commit but in a sub transaction: model still in wait queue
-        self.session.commit()
-        assert state.to_update == [('new', contact)]
+    session.add(contact)
+    # no commit: model is in wait queue
+    session.flush()
+    assert state.to_update == [('new', contact)]
 
-        # 'final' commit: models sent for indexing update
-        self.session.commit()
-        assert state.to_update == []
+    # commit but in a sub transaction: model still in wait queue
+    session.commit()
+    assert state.to_update == [('new', contact)]
 
-    def test_clear(self):
-        # just check no exception happens
-        self.svc.clear()
+    # 'final' commit: models sent for indexing update
+    session.commit()
+    assert state.to_update == []
 
-        # check no double stop (would raise AssertionError from service base)
-        self.svc.start()
-        self.svc.stop()
-        self.svc.clear()
+
+def test_clear(app, svc):
+    # just check no exception happens
+    svc.clear()
+
+    # check no double stop (would raise AssertionError from service base)
+    svc.start()
+    svc.stop()
+    svc.clear()
