c098a60ee669cfa5093a917a691206981e0a9ce6
Stefane Fermigier <sf@fermigier.com>
2018-02-15 13:29:46 +0100

    refactor test

--- abilian/web/tests/test_actions.py
+++ abilian/web/tests/test_actions.py
@@ -38,71 +38,84 @@ OTHER_CAT = Action(
 ALL_ACTIONS = (BASIC, CONDITIONAL, OTHER_CAT)
 
 
-class TestActions(BaseTestCase):
-    """Test Action and ActionRegistry."""
+def setup_actions(app):
+    actions.init_app(app)
+    for a in ALL_ACTIONS:
+        a.enabled = True
+    actions.register(*ALL_ACTIONS)
+    actions._init_context(app)
+    actions.context['show_all'] = True
 
-    def setUp(self):
-        BaseTestCase.setUp(self)
-        actions.init_app(self.app)
-        for a in ALL_ACTIONS:
-            a.enabled = True
-        actions.register(*ALL_ACTIONS)
-        actions._init_context(self.app)
-        actions.context['show_all'] = True
 
-    def test_installed(self):
-        assert actions.installed()  # test current_app (==self.app)
-        assert actions.installed(self.app)
-        assert not actions.installed(Flask('dummyapp'))
+def test_installed(app, app_context):
+    assert actions.installed()  # test current_app (==self.app)
+    assert actions.installed(app)
+    assert not actions.installed(Flask('dummyapp'))
 
-    def test_actions(self):
-        all_actions = actions.actions()
-        assert 'cat_1' in all_actions
-        assert 'cat_2:sub' in all_actions
-        assert all_actions['cat_1'] == [BASIC, CONDITIONAL]
-        assert all_actions['cat_2:sub'] == [OTHER_CAT]
 
-    def test_for_category(self):
-        cat_1 = actions.for_category('cat_1')
-        assert cat_1 == [BASIC, CONDITIONAL]
+def test_actions(app, app_context):
+    setup_actions(app)
 
-        cat_2 = actions.for_category('cat_2:sub')
-        assert cat_2 == [OTHER_CAT]
+    all_actions = actions.actions()
+    assert 'cat_1' in all_actions
+    assert 'cat_2:sub' in all_actions
+    assert all_actions['cat_1'] == [BASIC, CONDITIONAL]
+    assert all_actions['cat_2:sub'] == [OTHER_CAT]
 
-    def test_conditional(self):
-        actions.context['show_all'] = False
-        assert actions.for_category('cat_1') == [BASIC]
 
-    def test_enabled(self):
-        assert CONDITIONAL.enabled
-        assert actions.for_category('cat_1') == [BASIC, CONDITIONAL]
+def test_for_category(app, app_context):
+    setup_actions(app)
 
-        CONDITIONAL.enabled = False
-        assert not CONDITIONAL.enabled
-        assert actions.for_category('cat_1') == [BASIC]
+    cat_1 = actions.for_category('cat_1')
+    assert cat_1 == [BASIC, CONDITIONAL]
 
-    def test_action_url_from_context(self):
-        url = OTHER_CAT.url({'for': 'having', '2 keys': 'in context'})
-        assert url == 'http://count?2'
-        assert OTHER_CAT.url({}) == 'http://count?0'
+    cat_2 = actions.for_category('cat_2:sub')
+    assert cat_2 == [OTHER_CAT]
 
-    def test_render(self):
-        assert BASIC.render() == Markup(
-            '<a class="action action-cat_1 action-cat_1-basic" '
-            'href="http://some.where">'
-            '<i class="glyphicon glyphicon-ok"></i> Basic Action</a>',
-        )
 
-        assert CONDITIONAL.render() == Markup(
-            '<a class="action action-cat_1 action-cat_1-conditional '
-            'btn btn-warning" href="http://condition.al">'
-            '<i class="glyphicon glyphicon-hand-right"></i> '
-            'Conditional Action</a>',
-        )
+def test_conditional(app, app_context):
+    setup_actions(app)
 
-        assert OTHER_CAT.render() == Markup(
-            '<a class="action action-cat_2-sub action-cat_2-sub-other '
-            'custom-class" href="http://count?3">'
-            '<img src="/static/icons/other.png" width="14" height="14" /> '
-            'Other Action</a>',
-        )
+    actions.context['show_all'] = False
+    assert actions.for_category('cat_1') == [BASIC]
+
+
+def test_enabled(app, app_context):
+    setup_actions(app)
+
+    assert CONDITIONAL.enabled
+    assert actions.for_category('cat_1') == [BASIC, CONDITIONAL]
+
+    CONDITIONAL.enabled = False
+    assert not CONDITIONAL.enabled
+    assert actions.for_category('cat_1') == [BASIC]
+
+
+def test_action_url_from_context():
+    url = OTHER_CAT.url({'for': 'having', '2 keys': 'in context'})
+    assert url == 'http://count?2'
+    assert OTHER_CAT.url({}) == 'http://count?0'
+
+
+def test_render(app, test_request_context):
+    setup_actions(app)
+
+    assert BASIC.render() == Markup(
+        '<a class="action action-cat_1 action-cat_1-basic" '
+        'href="http://some.where">'
+        '<i class="glyphicon glyphicon-ok"></i> Basic Action</a>',
+    )
+
+    assert CONDITIONAL.render() == Markup(
+        '<a class="action action-cat_1 action-cat_1-conditional '
+        'btn btn-warning" href="http://condition.al">'
+        '<i class="glyphicon glyphicon-hand-right"></i> '
+        'Conditional Action</a>',
+    )
+
+    assert OTHER_CAT.render() == Markup(
+        '<a class="action action-cat_2-sub action-cat_2-sub-other '
+        'custom-class" href="http://count?3">'
+        '<img src="/static/icons/other.png" width="14" height="14" /> '
+        'Other Action</a>',
+    )
