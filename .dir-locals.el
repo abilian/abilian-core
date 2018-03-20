92f5111d6f2627b083a4114936885cd0d1b77eb7
Stefane Fermigier <sf@fermigier.com>
2018-03-20 10:05:32 +0100

    tooling: no need for a specific emacs config actually.

--- .dir-locals.el
+++
@@ -1,8 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((python-mode
-  (py-indent-offset . 4)
-  (py-continuation-offset . 8)
-  (py-smart-indentation)
-  (flycheck-pylintrc . "pylintrc")))
