e27e596daa7cdcf2f695b9437a10ee8730e8e965
Stefane Fermigier <sf@fermigier.com>
2018-03-20 10:04:04 +0100

    tooling: correct (?) emacs config.

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,14 +2,7 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((python-mode
-  (python-indent-offset . 4)
-  (py-continuation-offset . 4)
+  (py-indent-offset . 4)
+  (py-continuation-offset . 8)
   (py-smart-indentation)
-  (py-indent-offset . 2)
   (flycheck-pylintrc . "pylintrc")))
-
-
-
-
-
-
