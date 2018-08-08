ab7712f5814df9fbc89fcba60bf3e3a6299dce22
Stefane Fermigier <sf@fermigier.com>
2018-08-08 20:07:10 +0200

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,22 @@
 module.exports = {
   root: true,
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: [
-    'standard',
-    "plugin:requirejs/recommended",
-  ],
-  // 'html': required to lint *.vue files
-  plugins: [
-    'html', 'requirejs'
-  ],
+  extends: ["standard", "plugin:requirejs/recommended"],
+  // required to lint *.vue files
+  plugins: ["html", "requirejs"],
   env: {
     browser: true,
     es6: true,
   },
   // add your custom rules here
   rules: {
-    // Our own deviations from 'standard'
-    'quotes': 'off',
-    'semi': ['error', 'always'],
-    'comma-dangle': ['error', 'always-multiline'],
-    'space-before-function-paren': 'off',
-    'camelcase': 'off',
+    quotes: ["error", "double", { avoidEscape: true }],
+    semi: ["error", "always"],
+    "comma-dangle": ["error", "always-multiline"],
+    "space-before-function-paren": ["error", "never"],
+    camelcase: "off",
+    curly: "error",
     // TODO: 1 occurrence that seems fishy
     'handle-callback-err': 'off',
-    // TODO: 1 occurrence that seems fishy
-    // 'no-new': 'off',
   },
 };
