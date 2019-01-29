a6e2f40e2510426f0249b96157eb3dabfe89e7e7
Stefane Fermigier <sf@fermigier.com>
2019-01-29 09:27:14 +0100

    remove old testing code

--- abilian/testing/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-"""Elements to build test cases for an :class:`abilian.app.Application`"""
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-from .base import BaseTestCase, TestConfig
-
-__all__ = ("BaseTestCase", "TestConfig")
